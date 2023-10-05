21439e90c89dea2f33899d9cd8e714ecfc61c427
lunedg <lune.degroote@cegeka.com>
2023-10-05 15:27:11 +0200

    soldiers name

--- src/main/java/be/cegeka/battle/Soldier.java
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -8,10 +8,9 @@ public class Soldier {
     private final String name;
 
     public Soldier(String name) {
-        if (Strings.isNullOrEmpty(name)) {
-            throw new IllegalArgumentException();
+        if (Strings.isNullOrEmpty(name) || name.isBlank()) {
+            throw new IllegalArgumentException("A soldier must have a name");
         }
-
         this.name = name;
     }
 
